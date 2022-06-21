59386091a35c5579f18d963ce612034851083bca
sahityakarn <sahityakarn@gmail.com>
2022-06-21 19:23:00 +0530

    secound

--- resources/views/admin/branch/branchadmin.blade.php
+++ resources/views/admin/branch/branchadmin.blade.php
@@ -41,10 +41,8 @@
     <script src="{{ asset('assets/vendors/jszip/dist/jszip.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/pdfmake/build/pdfmake.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/pdfmake/build/vfs_fonts.js') }}"></script>
-
     <!-- iCheck -->
     <script src="{{ asset('assets/vendors/iCheck/icheck.min.js') }}"></script>
-
     <!-- jquery.inputmask -->
     <script src="{{ asset('assets/vendors/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js') }}"></script>
     <!-- Barnch js -->
@@ -62,31 +60,6 @@
             });
         });
     </script>
-
-    {{-- <!-- bootstrap-progressbar -->
-    <script src="{{ asset('assets/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js') }}"></script>
-   
-    <!-- bootstrap-daterangepicker -->
-    <script src="{{ asset('assets/vendors/moment/min/moment.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
-    <!-- bootstrap-wysiwyg -->
-    <script src="{{ asset('assets/vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js') }}"></script>
-    <script src="{{ asset('assets/vendors/jquery.hotkeys/jquery.hotkeys.js') }}"></script>
-    <script src="{{ asset('assets/vendors/google-code-prettify/src/prettify.js') }}"></script>
-    <!-- jQuery Tags Input -->
-    <script src="{{ asset('assets/vendors/jquery.tagsinput/src/jquery.tagsinput.js') }}"></script>
-    <!-- Switchery -->
-    <script src="{{ asset('assets/vendors/switchery/dist/switchery.min.js') }}"></script>
-    <!-- Select2 -->
-    <script src="{{ asset('assets/vendors/select2/dist/js/select2.full.min.js') }}"></script>
-    <!-- Parsley -->
-    <script src="{{ asset('assets/vendors/parsleyjs/dist/parsley.min.js') }}"></script>
-    <!-- Autosize -->
-    <script src="{{ asset('assets/vendors/autosize/dist/autosize.min.js') }}"></script>
-    <!-- jQuery autocomplete -->
-    <script src="{{ asset('assets/vendors/devbridge-autocomplete/dist/jquery.autocomplete.min.js') }}"></script>
-    <!-- starrr -->
-    <script src="{{ asset('assets/vendors/starrr/dist/starrr.js') }}"></script> --}}
 @endsection
 @section('body-container')
     <div class="right_col" role="main">
